3d01414af1c7326c13cf9263096ecc15ab1856d5
Guilherme Henrique <guilhermehenrique.fju@outlook.com>
2023-02-24 17:15:49 -0300

    JavaScript Na Prática 01 (Origamid)

---
+++ Variáveis/Tipos de Dados/script.js
@@ -0,0 +1,35 @@
+// Declare uma variável contendo uma string
+
+var nome = 'André'
+console.log(nome)
+
+// Declare uma variável contendo um número dentro de uma string
+
+var frase = 'André acertou ' + 8 + ' cestas no basquete'
+var frase1 = `André acertou ${9} cestas no basquete`
+console.log(frase, frase1)
+
+// Declare uma variável com a sua idade
+
+var idade = 23
+console.log(idade)
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var Nome = 'Guilherme',
+sobrenome = 'Henrique';
+
+var soma = Nome +  ' ' + sobrenome
+console.log(soma)
+var soma1 = `${Nome} ${sobrenome}`
+console.log(soma1)
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var Frase = "It's time"
+console.log(Frase)
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof Nome)
---
+++ Variáveis/script.js
@@ -0,0 +1,12 @@
+var nome = 'Guilherme',
+idade = 23,
+n1,
+n2,
+n3,
+n4,
+n5;
+
+var ComidaFavorita
+ComidaFavorita = 'Pizza Doce'
+
+console.log(nome, idade, ComidaFavorita, n1, n2, n3, n4, n5)
