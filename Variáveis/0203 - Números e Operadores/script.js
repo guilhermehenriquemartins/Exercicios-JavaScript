13f678c5acdfaae5569c2544d7b55f28e76f656b
Guilherme Henrique <guilhermehenrique.fju@outlook.com>
2023-02-26 15:36:44 -0300

    Aula de Números e Operadores (Parte 1 e Parte 2)
    Conteúdo visto na Origamid

---
+++ Variáveis/0203 - Números e Operadores/script.js
@@ -0,0 +1,34 @@
+//Exercício1
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+//Exercício2
+var nome = 'Guilherme'
+var idade = 18
+var resultado = nome / idade
+console.log(resultado)
+
+var resultado2 = 'Ele é soma de 5 e' * 8
+console.log(resultado2)
+
+//Exercício3
+var n1 = '200'
+var n2 = 50
+var res = +n1 + n2
+console.log(res)
+
+//Exercício4
+var num1 = 5
+console.log(num1++)
+console.log(num1)
+
+var num2 = 8
+console.log(++num2)
+
+//Exercício5
+var numero = +'80' / 2,
+unidade = 'kg',
+peso = numero + unidade,
+pesoPorDois = peso
+
+console.log(pesoPorDois)
